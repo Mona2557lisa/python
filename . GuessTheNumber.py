94ccd1779a7defe02a188c5945978a0d8c8cf702
Monalisa.N <monalishan.24.beai@acharya.ac.in>
2025-05-25 14:07:33 +0200

    GuessTheNumber.py

--- . GuessTheNumber.py
+++ . GuessTheNumber.py
@@ -1,26 +1,14 @@
-print("name : monalisa.n \n usn : 1AY24AI073 \n section : O ")
 import random
-def guess_the_number():
-    print("Welcome to Guess the Number!")
-    print("I'm thinking of a number between 1 and 100.")
 
-    number_to_guess = random.randint(1, 100)
-    attempts = 0
+print("monalisa.n, USN: 1AY24AI073, SEC: O")
+num = int(input("Enter the number between 1 to 9: "))
 
-    while True:
-        try:
-            guess = int(input("Take a guess: "))
-            attempts += 1
-
-            if guess < number_to_guess:
-                print("Too low! Try again.")
-            elif guess > number_to_guess:
-                print("Too high! Try again.")
-            else:
-                print(f"Congratulations! You guessed the number in {attempts} tries.")
-                break
-        except ValueError:
-            print("Please enter a valid number.")
-
-if __name__ == "__main__":
-    guess_the_number()
+while num < 1 or num > 9:
+  num = int(input("Invalid input! Please enter a numberbetween 1 to 9: "))
+  print(num, "is the number entered by user")
+  comp_num = random.randint(1, 9)
+  print(comp_num, "is the number randomly taken bycomputer")
+    
+if num == comp_num:
+  print("User guess is Absolutely Correct!!!!!!!!!!")
+else: print("Oh NO! User guess isIncorrect!!!!!!!!!!")
